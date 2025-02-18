f348f915e1c6899b036289d8ef3134df9fdad805
Carmelo Marsana <carmelo.marsana@gmail.com>
2025-02-18 12:14:49 +0100

    project init + frontend

---
+++ src/main/java/me/carmelo/theforums/TheForumsApplication.java
@@ -0,0 +1,13 @@
+package me.carmelo.theforums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheForumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheForumsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/carmelo/theforums/TheForumsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.carmelo.theforums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheForumsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
